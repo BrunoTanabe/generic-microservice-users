daa760719e80c873cf26c1747a73a16020fb347e
Bruno Tanabe <tanabebruno@gmail.com>
2025-04-18 17:37:31 -0300

    [BUILD]: Initialize Generic Microservice Users application with basic structure and configuration, using Java Spring Boot

---
+++ src/main/java/io/github/brunotanabe/generic_microservice_users/GenericMicroserviceUsersApplication.java
@@ -0,0 +1,13 @@
+package io.github.brunotanabe.generic_microservice_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericMicroserviceUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericMicroserviceUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/brunotanabe/generic_microservice_users/GenericMicroserviceUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunotanabe.generic_microservice_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericMicroserviceUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
